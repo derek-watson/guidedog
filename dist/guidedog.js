5b3bcc66f453623101d8ebc2e0921905c0ebe7ff
Derek Watson <watson@dcw.ca>
2014-10-14 00:50:30 -0400

    switched back to js.yaml

--- dist/guidedog.js
+++ dist/guidedog.js
@@ -1037,6 +1037,7 @@ exports.eval = function(str) {
 },{}]},{},[1])
 (function($){
   $.fn.guidedog = function(options, callback){
+
     // default options
     var settings = $.extend({
       cssPaths: ['/css/app.css'],
@@ -1047,139 +1048,45 @@ exports.eval = function(str) {
     var gd = {}               // stores info to be passed to handlebars template
     var combinedResponse = '' // store all of the raw content to be parsed
     var crawlTotal = settings.cssPaths.length - 1
+
     gd.data = {}
     gd.data.info = {}
     gd.data.sections = []
 
-    // root initializer
-    var init = function(){
-      fetch(0)
-    }()
+    // fetch and combine all cssPaths' contents
+    var deferreds = []
+    $.each(settings.cssPaths, function(i, path) {
+      deferred.push($.get(path))
+    })
 
-    // crawl through and combine all cssPaths' contents
-    function fetch(crawlCnt){
-      $.when($.get(settings.cssPaths[crawlCnt])).done(function(response) {
-        combinedResponse += response
-        if (crawlCnt === crawlTotal){
-          parse()
-        }
-        else{
-          fetch(crawlCnt + 1)
-        }
-      })
-    }
-
-    // parse all of the raw content
-    var parse = function(){
-      var expression = /\/\*\!\!([\s\S]*?)\*\//mg
-      while ((match = expression.exec(combinedResponse)) != null){
-        // work with !!! or !! (Less.js support)
-        if(match[1].substring(0,1) == '!'){
-          match[1] = match[1].substring(1)
-        }
-        match[1] = lorem(match[1]);
-
-        console.log(match[1]);
-        match = jsyaml.load(match[1])
-        console.log(match);
-
-        if(match.description) match.description = markdown.toHTML(match.description)
-        if (new_section(match.section)){
-          var section = match.section
-          if(!match.section) match.section = 'Undefined'
-          gd.data.sections.push({
-            "section": match.section,
-            "id": match.section.replace(/\s+/g, '').toLowerCase(),
-            "subSections": []
-          })
-        }
-        var index = sectionIndex(match.section)
-        gd.data.sections[index].subSections.push(match)
-      }
-      gd.data.info.logo = settings.logoPath;
-      render()
-    }
-
-    // expand lorem ipsum
-    var lorem = function(str){
-      var words = ['lorem','ipsum','dolor','sit','amet','consectetuer','adipiscing','elit','sed','diam','nonummy','nibh','euismod', 'tincidunt','ut','laoreet']
-      var wordsCnt = words.length-1;
-      var expression = /lorem[^\s]+/g
-      return str.replace(expression, function(cnt, index){
-        cnt = parseInt(cnt.replace('lorem', ''))
-        var paragraph = ''
-        for(var b=1; b<=cnt; b++){
-          var sentence = ''
-          var sentenceLength = rand(12,16);
-          for(var i=0; i<=sentenceLength; i++){
-            sentence += ' ' + words[rand(0, wordsCnt)]
-            if(i==0){
-              sentence = sentence.substr(1)
-              sentence = sentence.charAt(0).toUpperCase() + sentence.slice(1)
-            }
-            else if(i==sentenceLength){
-              sentence += '. '
-              paragraph += sentence
-            }
-          }
-        }
-        return paragraph;
-      });
-    }
-
-    // return a random int within a range
-    var rand = function(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+    $.when.apply($, deferreds).done(function() {
+      var args = Array.prototype.slice.call(arguments)
+      combinedResponse = args.join('')
+    })
 
     // pass json data to Mustache template
     var render = function(){
       gdDOM.html('').prepend(Guidedog.templates.guidedog(gd.data, {}));
       compileJade();
-      $('.guidedog').each(function(){ $(this).css('background', '#'+Math.floor(Math.random()*16777215).toString(16)); });
-      syntaxHighlight();
+      $('.guidedog').each(function(){
+        $(this).css('background', '#'+Math.floor(Math.random()*16777215).toString(16));
+      });
       updateNav();
       scrollToClick();
       initTabs();
-      $(document).on('scroll', function(){updateNav();});
+
+      $(document).on('scroll', function(){
+        updateNav();
+      });
+
       if (typeof callback == 'function') {
         callback.call(gdDOM);
       }
+
       if(window.location.hash){
         // bug causing this not to work properly in Chrome
         scrollTo(window.location.hash, 0);
       }
-    }
-
-    // syntax highlight markup
-    var syntaxHighlight = function(){
-      Prism.highlightAll();
-    }
-
-    // compile jade examples
-    var compileJade = function(){
-      $('.sg-jade').each(function(){
-        // define targets
-        var target = $(this)
-        var targetJade = target.find('.language-haskell')
-        var targetHTML = target.find('.language-markup')
-        var targetExample = target.find('.sg-example')
-
-        // capture jade
-        var stringJade = targetJade.text();
-        stringJade = stringJade.replace(/^\s+|\s+$/g,'')
-
-        // convert to html
-        var stringHTML = jade.compile(stringJade, {
-          pretty: true
-        })()
-
-        // render html example
-        targetExample.html(stringHTML)
-
-        // render html code sample
-        targetHTML.text(stringHTML)
-      });
     }
 
     // update active nav item
@@ -1226,16 +1133,6 @@ exports.eval = function(str) {
         }
       }
       return 0
-    }
-
-    // determine if a section already exists
-    var new_section = function(section){
-      for (key in gd.data.sections){
-        if(section === gd.data.sections[key].section){
-          return false
-        }
-      }
-      return true
     }
 
     // init sg-tabs
